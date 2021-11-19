2f1b81e6bad2306f879cd105a632e540addffcfa
Firito <yuzhefirito@gmail.com>
2021-11-19 19:58:11 +0800

    [modify] 增加PPTX解析的设置透明色实现方法；

---
+++ Learnland.DotNetCore.PptxAnalysis/Image/SetTransparentColor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using Learnland.DotNetCore.Extensions;
+
+namespace Learnland.DotNetCore.PptxAnalysis.Image
+{
+    /// <summary>
+    ///     设置透明色
+    /// </summary>
+    public class SetTransparentColor
+    {
+        /// <summary>
+        ///     将图片<paramref name="bitmap"/>上指定的颜色<paramref name="colorA"/>替换为颜色<paramref name="colorB"/>
+        /// </summary>
+        /// <param name="bitmap">图片</param>
+        /// <param name="colorA">要被替换的颜色</param>
+        /// <param name="colorB">要将<paramref name="colorA"/>替换的成颜色</param>
+        public void ReplaceColor(Bitmap bitmap, Color colorA, Color colorB)
+        {
+            //这里是遍历图片中的每一个像素
+            bitmap.PerPixelProcess(color =>
+            {
+                //如果当前的颜色和颜色colorA近似，则进行替换
+                var isSimilar = IsSimilarColors(color, colorA);
+                return isSimilar ? colorB : color;
+            });
+        }
+
+        /// <summary>
+        ///     是否是近似颜色
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="accuracy">Rgb通道允许的误差</param>
+        /// <returns></returns>
+        private bool IsSimilarColors(Color x, Color y, int accuracy = 36)
+        {
+            var offsetA = x.A - y.A;
+            var offsetR = x.R - y.R;
+            var offsetG = x.G - y.G;
+            var offsetB = x.B - y.B;
+
+            if (Math.Abs(offsetA) > 1)
+            {
+                return false;
+            }
+
+            if (offsetR == offsetG && offsetR == offsetB)
+            {
+                if (Math.Abs(offsetR) > 1)
+                {
+                    return ColorDifference(x, y) <= accuracy / 3d;
+                }
+            }
+
+            var difference = ColorDifference(x, y);
+            return difference <= accuracy;
+        }
+
+        /// <summary>
+        /// 在RGB空间上通过公式计算出加权的欧式距离
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private double ColorDifference(Color x, Color y)
+        {
+            var m = (x.R + y.R) / 2.0;
+            var r = Math.Pow(x.R - y.R, 2);
+            var g = Math.Pow(x.G - y.G, 2);
+            var b = Math.Pow(x.B - y.B, 2);
+
+            return Math.Sqrt((2 + m / 256) * r + 4 * g + (2 + (255 - m) / 256) * b);
+        }
+    }
+}
---
+++ Learnland.DotNetCore/Extensions/BitmapExtension.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Learnland.DotNetCore.Extensions
+{
+    /// <summary>
+    ///     提供<see cref="Bitmap"/>使用的拓展方法
+    /// </summary>
+    public static class BitmapExtension
+    {
+        /// <summary>
+        ///     对图像进行逐像素处理
+        /// </summary>
+        /// <param name="bitmap">源图像</param>
+        /// <param name="func">处理回调函数</param>
+        public static void PerPixelProcess(this Bitmap bitmap, Func<Color, Color> func)
+        {
+            var pixelFormat = bitmap.PixelFormat;
+
+            if (pixelFormat != PixelFormat.Format32bppArgb && pixelFormat != PixelFormat.Format24bppRgb)
+            {
+                throw new NotSupportedException($"Unsupported image pixel format {nameof(pixelFormat)} is used.");
+            }
+
+            var cols = bitmap.Width;
+            var rows = bitmap.Height;
+            var channels = Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            var total = cols * rows * channels;
+
+            //锁定图片并拷贝图片像素
+            var rect = new Rectangle(0, 0, cols, rows);
+            var bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            var iPtr = bitmapData.Scan0;
+            var data = new byte[total];
+            Marshal.Copy(iPtr, data, 0, total);
+
+            //逐像素处理
+            Parallel.For(0, rows, row =>
+            {
+                for (var col = 0; col < cols; col++)
+                {
+                    var indexOffset = (row * cols + col) * channels;
+                    if (!TryCreateColorFromData(data, indexOffset, channels, out var color)) continue;
+
+                    var targetColor = func?.Invoke(color);
+                    if (targetColor != null)
+                    {
+                        SaveToData(targetColor.Value, data, indexOffset, channels);
+                    }
+                }
+            });
+
+            Marshal.Copy(data, 0, iPtr, total);
+            bitmap.UnlockBits(bitmapData);
+        }
+
+        /// <summary>
+        ///     尝试获取颜色
+        /// </summary>
+        /// <param name="colorData"></param>
+        /// <param name="offset"></param>
+        /// <param name="channels"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        private static bool TryCreateColorFromData(byte[] colorData, int offset, int channels, out Color color)
+        {
+            //需要考虑大小端
+            var isLittleEndian = BitConverter.IsLittleEndian;
+            color = Color.Black;
+
+            try
+            {
+                if (channels == 3)
+                {
+                    var r = isLittleEndian ? colorData[offset + 2] : colorData[offset];
+                    var g = colorData[offset + 1];
+                    var b = isLittleEndian ? colorData[offset] : colorData[offset + 2];
+                    color = Color.FromArgb(byte.MaxValue, r, g, b);
+                    return true;
+                }
+
+                if (channels == 4)
+                {
+                    var a = isLittleEndian ? colorData[offset + 3] : colorData[offset + 0];
+                    var r = isLittleEndian ? colorData[offset + 2] : colorData[offset + 1];
+                    var g = isLittleEndian ? colorData[offset + 1] : colorData[offset + 2];
+                    var b = isLittleEndian ? colorData[offset + 0] : colorData[offset + 3];
+                    color = Color.FromArgb(a, r, g, b);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     保存颜色到数组
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="colorData"></param>
+        /// <param name="offset"></param>
+        /// <param name="channels"></param>
+        /// <returns></returns>
+        private static void SaveToData(Color color, byte[] colorData, int offset, int channels)
+        {
+            //需要考虑大小端
+            var isLittleEndian = BitConverter.IsLittleEndian;
+
+            if (channels == 3)
+            {
+                colorData[offset] = isLittleEndian ? color.B : color.R;
+                colorData[offset + 1] = color.G;
+                colorData[offset + 2] = isLittleEndian ? color.R : color.B;
+            }
+
+            if (channels == 4)
+            {
+                colorData[offset + 0] = isLittleEndian ? color.B : color.A;
+                colorData[offset + 1] = isLittleEndian ? color.G : color.R;
+                colorData[offset + 2] = isLittleEndian ? color.R : color.G;
+                colorData[offset + 3] = isLittleEndian ? color.A : color.B;
+            }
+        }
+    }
+}
