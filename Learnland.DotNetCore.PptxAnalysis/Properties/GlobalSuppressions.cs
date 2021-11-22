526168196f3332ac2e9b770d16ec87b02cdaa387
Firito <yuzhefirito@gmail.com>
2021-11-22 11:46:59 +0800

    [modify] 增加PPTX解析的柔化边缘效果的实现方法；

---
+++ Learnland.DotNetCore.PptxAnalysis/Properties/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:验证平台兼容性", Justification = "<挂起>", Scope = "member", Target = "~M:Learnland.DotNetCore.PptxAnalysis.Image.ImageEffect.SetSoftEdgeEffect(System.Drawing.Bitmap,System.Single)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:验证平台兼容性", Justification = "<挂起>", Scope = "member", Target = "~M:Learnland.DotNetCore.PptxAnalysis.Image.ImageEffect.CreateSoftEdgeBitmap(System.Drawing.Bitmap,System.Single)~System.Drawing.Bitmap")]
